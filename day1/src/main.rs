2a3d50159d17dc1021e49635992fca883d40285f
Marshall Polaris <marshall@pol.rs>
2024-11-30 23:07:14 -0800

    Even better day 1 Rust

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,16 +1,23 @@
 use itertools::Itertools;
 use std::collections::HashMap;
 use std::error::Error;
-use std::io::{self, Read};
+use std::io;
+
+fn parse_pair(line: &str) -> Result<(usize, usize), Box<dyn Error>> {
+    if let Some((l, r)) = line.split_whitespace().collect_tuple() {
+        Ok((l.parse()?, r.parse()?))
+    } else {
+        Err(format!("Failed to parse line: {}", line).into())
+    }
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut input = String::new();
-    io::stdin().read_to_string(&mut input)?;
     let mut nums = Vec::<usize>::new();
     let mut cts = HashMap::<usize, usize>::new();
-    for (l, r) in input.split_whitespace().tuples() {
-        nums.push(l.parse()?);
-        cts.entry(r.parse()?).and_modify(|ct| *ct += 1).or_insert(1);
+    for line in io::stdin().lines() {
+        let (l, r) = parse_pair(&line?)?;
+        nums.push(l);
+        cts.entry(r).and_modify(|ct| *ct += 1).or_insert(1);
     }
     let total: usize = nums.iter().map(|n| n * cts.get(n).unwrap_or(&0)).sum();
     println!("{}", total);
