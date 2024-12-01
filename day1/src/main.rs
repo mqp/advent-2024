873bf7f901c056d4f51863e41f4f5177cddb38fe
Marshall Polaris <marshall@pol.rs>
2024-11-30 21:41:53 -0800

    Make the code look nice

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use std::collections::HashMap;
 use std::error::Error;
 use std::io::{self, Read};
@@ -5,27 +6,13 @@ use std::io::{self, Read};
 fn main() -> Result<(), Box<dyn Error>> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
-    let mut nums = Vec::<i32>::new();
-    for num in input.split_whitespace() {
-        nums.push(num.parse()?);
+    let mut nums = Vec::<usize>::new();
+    let mut cts = HashMap::<usize, usize>::new();
+    for (l, r) in input.split_whitespace().tuples() {
+        nums.push(l.parse()?);
+        cts.entry(r.parse()?).and_modify(|ct| *ct += 1).or_insert(1);
     }
-    let mut left = Vec::<i32>::new();
-    let mut right = Vec::<i32>::new();
-    for i in 0..(nums.len() / 2) {
-        let l = nums[2 * i];
-        let r = nums[2 * i + 1];
-        left.push(l);
-        right.push(r);
-    }
-    let mut counts = HashMap::new();
-    for n in right {
-        counts.entry(n).and_modify(|count| *count += 1).or_insert(1);
-    }
-    let mut total = 0;
-    for n in left {
-        let count = counts.get(&n).unwrap_or_else(|| &0);
-        total += count * n;
-    }
+    let total: usize = nums.iter().map(|n| n * cts.get(n).unwrap_or(&0)).sum();
     println!("{}", total);
     Ok(())
 }
